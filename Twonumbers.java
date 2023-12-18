4c7dfd70036c8051af6fc31dbd0806797cb3cd9d
Shaik Rufiya <150911765+Rufiya09@users.noreply.github.com>
2023-12-18 15:19:14 +0530

    2) Write a Java Program to Print whether two number's are equal or not

---
+++ Twonumbers.java
@@ -0,0 +1,33 @@
+//Write a java program to print whether two numbers are equal or not //
+
+
+class Twonumbers
+{
+	public static void main(String args[])
+	{
+		int num1 = 25;
+		int num2 = 45;
+		
+		if(num1==num2){
+			
+			System.out.println("the numbers are equal");
+		}
+		
+		else{ 
+			
+		    System.out.println("the numbers are not equal");
+		}
+	
+	    int num3 = 99;
+		int num4 = 99;
+		
+		if(num3==num4){
+			System.out.println("the numbers are equal");
+		}
+		else{
+			System.out.println("the numbers are not equal");
+		}
+		
+	
+	}
+}
